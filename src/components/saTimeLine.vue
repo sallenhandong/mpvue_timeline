1f5388c69364c8b66037c57980e33f9b91bc6fe7
Slim Sallen <296472276@qq.com>
2019-02-21 12:30:28 +0800

    first commit

---
+++ src/components/saTimeLine.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div class="listDiv">
+      <div :class="index==0?'listDivItemFirst':'listDivItem'">
+        <div :class="obj.isSelect=='1'?'circleSelect':'circle'">{{index + 1}}</div>
+        <div class="timeLinelineTitle">{{obj.title}}</div>
+      </div>
+      <div :class="index==maxLength-1?'listDivItem1End':'listDivItem1'">
+        <div  :class="index==maxLength-1?'timeLinelineEnd':'timeLineline'" v-bind:style="{ backgroundColor: lineColor }"></div>
+        <div class="timeLinecontent">{{obj.content}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+   data() {
+    return {
+    
+    };
+  },
+    //   "state":''
+    //   "title":''
+    //   "content":''
+    //   "index":''
+    //   "id",:''
+    //   "isSelect":''
+  props: ['obj','index','maxLength','lineColor'
+  ],
+  
+};
+</script>
+<style>
+.listDiv {
+  display: flex;
+  flex-direction: column;
+}
+.listDivItemFirst {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 40rpx;
+  margin-top: 40rpx;
+}
+
+.listDivItem {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 40rpx;
+}
+.listDivItem1 {
+  display: flex;
+}
+.listDivItem1End {
+  margin-bottom: 20rpx;
+  display: flex;
+}
+.timeLinelineTitle {
+  font-size: 30rpx;
+  color: #363636;
+  font-weight: 650;
+  margin-left: 20rpx;
+  margin-top: -10rpx;
+}
+.timeLinecontent {
+  font-size: 28rpx;
+  color: #999999;
+  padding-bottom: 20rpx;
+  width: 600rpx;
+}
+.circleSelect {
+  width: 60rpx;
+  height: 60rpx;
+  color: white;
+  background-color: rgba(0, 121, 254, 1);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.circle {
+  width: 60rpx;
+  height: 60rpx;
+  color: #999999;
+  background-color: white;
+  border-radius: 50%;
+  border: 2rpx solid lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.timeLineline {
+  width: 4rpx;
+  background-color: rgba(38, 156, 255, 1);
+  margin-left: 70rpx;
+  margin-right: 44rpx;
+  box-shadow: 0rpx 0rpx 2rpx rgba(53, 210, 250, 1);
+}
+.timeLinelineEnd {
+  height: 0rpx;
+  margin-left: 70rpx;
+  margin-right: 52rpx;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="bgView">
+    <div class="content">
+        <div class="progress">
+            <label for="" >timeline</label>
+        </div>
+        <div class="timeLineDiv">
+               <div v-for="(item, index) in listArr" :key="index">
+            <timeLine :obj='item' :index='index' :maxLength='listArr.length' ></timeLine>
+        </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+import timeLine from "@/components/saTimeLine";
+export default {
+  data() {
+    return {
+      title: "阿里巴巴",
+      content: "授权回话资源背景调查",
+       listArr: [
+        {
+          title: "setp 1",
+          content: "开始记录今天的生活",
+          id:'0',
+          isSelect:'1'
+        },
+        {
+          title: "setp 2",
+          content: "Know that I will live in my heart 深知我者才久居我心。",
+          id:'1',
+           isSelect:'0'
+        },{
+          title: "setp 3",
+          content: "To the time to life, rather than to life in time to the time to life, rather than to life in time.给时光以生命，而不是给生命以时光。",
+          id:'2',
+           isSelect:'0'
+        }
+      ]
+    };
+  },
+
+  components: {timeLine},
+
+  methods: {
+  
+  },
+  created() {
+    let app = getApp();
+  }
+};
+</script>
+
+<style scoped>
+.bgView {
+  background-color: rgba(245, 245, 245, 1);
+  height: 1206rpx;
+}
+.timeLineDiv {
+    background-color: white;
+    margin-top: 20rpx;
+}
+.progress {
+    margin-left: 40rpx;
+    margin-top: 20rpx;
+    color: #999999;
+}
+</style>
